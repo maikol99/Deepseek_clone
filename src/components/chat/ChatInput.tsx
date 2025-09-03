2b3bd89a8d53f86f0d1dbf982aa46330730e80ed
maikol <maikol@dev-maikol.fibertel.com.ar>
2025-09-03 14:04:46 -0300

    feat: add ChatInput component

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState, useRef, useEffect} from 'react'
+import { Textarea } from "@/components/ui/textarea"
 
-const ChatInput = () => {
+
+interface ChatInputProps{
+  onSubmit:(message:string) => void
+  isLoading:boolean
+}
+
+const ChatInput = ({onSubmit,isLoading}:ChatInputProps) => {
+  const [Input,setInput] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const [isSpeechActive, setIsSpeechActive] = useState(false)
+
+  const handleSubmit = (e:React.FormEvent) => {
+    e.preventDefault();
+    if(Input.trim()){
+      onSubmit(Input)
+      setInput("")
+    }
+  }
+
+  const handleKeyDown = (e:React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if(e.key === "Enter"&& !e.shiftKey){
+      e.preventDefault();
+      handleSubmit(e)
+    }
+
+  }
+
+  useEffect(() => {
+    const textarea = textareaRef.current;
+    if(textarea){
+      textarea.style.height = "auto";
+      textarea.style.height = `${Math.min(textarea.scrollHeight,150)}px`
+    }
+  },[Input])
+
   return (
-    <div>
-      ChatInput
+    <div className={'w-full max-w-4xl md:ml-64 rounded-xl bg-[#f4f4f6] py-4 px-4 shadow-[0_-1px_6px_rgba(0,0,0,0.05)]'}>
+      <form onSubmit={handleSubmit} className='w-full'>
+        <Textarea
+        ref={textareaRef}
+        placeholder=''
+
+        />
+      </form>
     </div>
   )
 }
