44b8ef521ee34af89f5a77d027bb364c630ab9f9
Maikol99 <alegremaikol21@gmail.com>
2025-09-26 20:22:32 -0300

    feat(auth): add login and register routes in AuthRoute.js

---
+++ backend/routes/AuthRoute.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const {register,login, getCurrentUser, googleCallback} = require('../controllers/authController');
+const authMiddleware = require('../middleware/AuthMiddleware');
+const passport = require('passport');
+
+const router = express.Router();
+
+
+//Register New user
+router.post('/register', register)
+//login users
+router.post('/login', login)
+//Obtener el usuario actual
+router.get('/me', authMiddleware,getCurrentUser)
+
+
+//Google Oauth routes
+router.get('/google',passport.authenticate('google'), { scope: ['profile','email']})
+
+router.get('google/callback/', passport.authenticate('google', {session:false}), { failuRedirect:'/sign-in'}, googleCallback)
+
+router.post('/logout', (req,res) =>{
+    res.clearCookie('auth_token',{
+        httpOnly:true,
+        saveSite:'none',
+        secure:true
+        
+    })
+     return res.status(201).json({message:"user logout success"})
+})
+
+module.exports= router;
+
